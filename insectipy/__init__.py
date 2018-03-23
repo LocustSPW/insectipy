1421aafbe247ddda7d5c2ac108cb9bab9b5a6850
Jeremy Huntwork <jhuntwork@lightcubesolutions.com>
2018-03-23 19:24:47 -0400

    Add a replica_set argument
      Assert that the Locusts class will initialize when passed an
      argument containing the replicaset to use

--- insectipy/__init__.py
+++ insectipy/__init__.py
@@ -2,4 +2,7 @@
 
 
 class Locusts(object):
-    pass
+    '''A class for interacting with the Locusts app'''
+
+    def __init__(self, replica_set):
+        self.replica_set = replica_set
--- tests/unit/test_Locusts.py
+++ tests/unit/test_Locusts.py
@@ -8,10 +8,18 @@ class LocustsTest(unittest.TestCase):
     '''Tests for the insectipy.Locusts Class'''
     def setUp(self):
         '''Set some pre-test state for each test in this class'''
+        self.replica_set = {
+            'name': 'testing',
+            'servers': [
+                'test1:27017',
+                'test2:27018',
+                'test3:27019'
+            ]
+        }
 
     def tearDown(self):
         '''clean up the state after each test'''
 
     def test_init(self):
         '''Test that we can instantiate a Locusts class'''
-        self.assertTrue(insectipy.Locusts())
+        self.assertTrue(insectipy.Locusts(self.replica_set))
